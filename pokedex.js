54731852ec97cb3cfe0063349109924213c79efe
illyasen1999 <mikquitain@gmail.com>
2022-04-22 14:53:11 +0800

    swtich case

--- pokedex.js
+++ pokedex.js
@@ -34,78 +34,104 @@ const getPokemon = () => {
         .insertAdjacentHTML("afterbegin", pokemonResult)
 
         // BG Color based on type
-        if(type == "???"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#68A090"
-        }
-        else if (type == "normal"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#68A090"
-        }
-
-        else if (type == "fire"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#F08030"
-        }
-
-        else if (type == "fighting"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#C03028"
-        }
-
-        else if (type == "water"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#6890F0"
-        }
-
-        else if (type == "flying"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#A890F0"
-        }
-
-        else if (type == "grass"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#78C850"
-        }
-        
-        else if (type == "poison"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#A040A0"
-        }
-
-        else if (type == "electric"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#F8D030"
-        }
-
-        else if (type == "ground"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#E0C068"
-        }
-
-        else if (type == "psychic"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#F85888"
-        }
-
-        else if (type == "rock"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#B8A038"
-        }
-
-        else if (type == "ice"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#98D8D8"
-        }
-
-        else if (type == "bug"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#A8B820"
-        }
-
-        else if (type == "dragon"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#7038F8"
-        }
-
-        else if (type == "ghost"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#705898"
-        }
-
-        else if (type == "dark"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#705848"
-        }
-
-        else if (type == "steel"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#B8B8D0"
-        }
-        else if (type == "fairy"){
-            document.querySelector(".pokemonList").style.backgroundColor = "#EE99AC"
+        switch(type){
+            case "???":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#68A090"
+                break;
+                
+            case "normal":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#68A090"
+                break;
+                
+            case "fire":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#F08030"
+                break;
+                
+            case "fighting":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#C03028"
+                break;
+                
+            case "water":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#6890F0"
+                break;
+                
+            case "flying":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#A890F0"
+                break;
+                
+            case "grass":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#78C850"
+                break;
+                
+            case "poison":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#A040A0"
+                break;
+                
+            case "electric":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#F8D030"
+                break;
+                
+            case "ground":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#E0C068"
+                break;
+                
+            case "psychic":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#F85888"
+                break;
+                
+            case "rock":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#B8A038"
+                break;
+                
+            case "ice":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#98D8D8"
+                break;
+                
+            case "bug":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#A8B820"
+                break;
+                
+            case "dragon":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#7038F8"
+                break;
+                
+            case "ghost":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#705898"
+                break;
+                
+            case "dark":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#705848"
+                break;
+                
+            case "steel":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#B8B8D0"
+                break;
+                
+            case "fairy":
+                document.querySelector(".pokemonList")
+                .style.backgroundColor = "#EE99AC"
+                break;
+                
+            default:
+                document.querySelector(".pokemonList").style.backgroundColor = "#000000"
         }
 
     })
@@ -120,4 +146,3 @@ document.querySelector("[name=pokemonName]").addEventListener("keyup", (e) => {
         getPokemon()
     }
 })
-
